b41b20c5823811c188d6548cf7b387bd5a89841b
Juliana Braga <jujbraga@gmail.com>
2025-05-23 09:48:17 +0200

    add js file

---
+++ script.js
@@ -0,0 +1,10 @@
+function toogleMode() {
+  const body = document.body
+  body.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  body.classList.contains("light")
+    ? img.setAttribute("src", "/assets/avatar-light.jpg")
+    : img.setAttribute("src", "/assets/avatar.jpg")
+}
